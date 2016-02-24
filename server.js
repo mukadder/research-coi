cf582e2be60e64f7ee9ec5a9d46e454133aba45b
Hitesh <hht179@gmail.com>
2016-02-24 10:06:48 -0500

    new file added 1006

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var node = new Server("boot the server").console.log('server running at port");
+var [] = ["8080",'8081'];
+var global = new Array("object",console);
 
+
+var console = "log this error";
+var login= function() {
+
+var legs=[];
+for(int i=0 i<legs.length; i++){
+legs.push(legs.splice(1,0);
+}
